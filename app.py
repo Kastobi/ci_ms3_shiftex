6e0010c3571493358a3fff9c3477ba159bc9ee54
apometricsTK <Tobias.Kast@t-online.de>
2021-04-02 16:21:29 +0200

    git cleanup

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import os
 
-<<<<<<< Updated upstream
-from flask import Flask
-=======
 from flask import Flask, render_template
->>>>>>> Stashed changes
 
 if os.path.exists("env.py"):
     import env
@@ -14,11 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-<<<<<<< Updated upstream
-    return "Hello shiftex"
-=======
     return render_template("index.html")
->>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
