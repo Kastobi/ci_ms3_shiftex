9dcf62eac72b2fa6b16f5830354b8b67277aa62c
apometricsTK <Tobias.Kast@t-online.de>
2021-06-09 18:27:32 +0200

    Fix: - Deployment issue

--- shiftex/config.py
+++ shiftex/config.py
@@ -6,7 +6,7 @@ if os.path.exists("env.py"):
 
 class Config:
     IP = os.environ.get("IP")
-    PORT = os.environ.get("PORT")
+    PORT = int(os.environ.get("PORT"))
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
