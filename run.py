48167fc15f55da7bb2ab9f1dbde2adec7bce2803
apometricsTK <Tobias.Kast@t-online.de>
2021-06-10 11:27:11 +0200

    Fix: - Deployment issue debug

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from shiftex import create_app
 
 app = create_app()
 
+# todo: debug mode ! switch to gunicorn for submission!
 if __name__ == "__main__":
     app.run()
-
-# todo: debug mode ! switch to gunicorn for submission!
