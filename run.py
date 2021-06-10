b911423d8a0cb2881d2f76438e236c64f6cccf43
apometricsTK <Tobias.Kast@t-online.de>
2021-06-10 11:43:01 +0200

    Fix: - Deployment issue debug

--- run.py
+++ run.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     app.run(
         host=os.getenv("IP"),
         port=os.getenv("PORT"),
-        debug=os.getenv("DEBUG")
+        debug=os.getenv("DEBUG"),
+        MONGO_URI=os.getenv("MONGO_URI")
     )
