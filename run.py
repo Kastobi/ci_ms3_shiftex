1a80bf18d44b5e0ecaf4c53b86d600e32e367b3b
apometricsTK <Tobias.Kast@t-online.de>
2021-06-10 12:10:52 +0200

    Fix: - Deployment issue debug

--- run.py
+++ run.py
@@ -9,8 +9,4 @@ app = create_app()
 
 # todo: debug mode ! switch to gunicorn for submission!
 if __name__ == "__main__":
-    app.run(
-        host=os.getenv("IP"),
-        port=os.getenv("PORT"),
-        debug=os.getenv("DEBUG")
-        )
+    app.run()
